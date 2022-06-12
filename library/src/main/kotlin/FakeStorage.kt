01909c0b7ef592a1b8f8b0b9e580d13697a8eea3
Dvir-David Biton <dbiton@campus.technion.ac.il>
2022-06-12 10:23:40 +0300

    added completefuture to FakeStorage and fakefactory

--- library/src/main/kotlin/FakeStorage.kt
+++ library/src/main/kotlin/FakeStorage.kt
@@ -1,15 +1,39 @@
 package il.ac.technion.cs.softwaredesign
 
 import il.ac.technion.cs.softwaredesign.storage.SecureStorage
-
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executors
 
 class FakeStorage public constructor() : SecureStorage {
     private var data : MutableMap<ByteArrayWrapper, ByteArray> = mutableMapOf(); //Dictionary
-    override fun read(key: ByteArray): ByteArray? {
-        return data[ByteArrayWrapper(key)]
+
+    @Throws(InterruptedException::class)
+    private fun readFuture(key: ByteArray): CompletableFuture<ByteArray?> {
+        val completableFuture = CompletableFuture<ByteArray?>()
+        Executors.newCachedThreadPool().submit<Any?> {
+            Thread.sleep(100)
+            completableFuture.complete(data[ByteArrayWrapper(key)])
+            null
+        }
+        return completableFuture
     }
 
-    override fun write(key: ByteArray, value: ByteArray) {
-        data[ByteArrayWrapper(key)] = value
+    @Throws(InterruptedException::class)
+    private fun writeFuture(key: ByteArray, value: ByteArray): CompletableFuture<Unit> {
+        val completableFuture = CompletableFuture<Unit>()
+        Executors.newCachedThreadPool().submit<Any?> {
+            Thread.sleep(100)
+            data[ByteArrayWrapper(key)] = value
+            null
+        }
+        return completableFuture
+    }
+
+    override fun read(key: ByteArray): CompletableFuture<ByteArray?> {
+        return readFuture(key)
+    }
+
+    override fun write(key: ByteArray, value: ByteArray): CompletableFuture<Unit> {
+        return writeFuture(key, value)
     }
 }
--- library/src/main/kotlin/SecureStorageDummy.kt
+++ library/src/main/kotlin/SecureStorageDummy.kt
@@ -2,12 +2,26 @@ package il.ac.technion.cs.softwaredesign
 
 import il.ac.technion.cs.softwaredesign.storage.SecureStorage
 import il.ac.technion.cs.softwaredesign.storage.SecureStorageFactory
+import java.util.concurrent.CompletableFuture
+import java.util.concurrent.Executors
 
 class FakeFactory : SecureStorageFactory {
     private var tables: MutableMap<ByteArrayWrapper, FakeStorage> = mutableMapOf(); //Dictionary
-    override fun open(name: ByteArray): SecureStorage {
-        tables[ByteArrayWrapper(name)] = FakeStorage()
-        return tables[ByteArrayWrapper(name)]!!
+
+    @Throws(InterruptedException::class)
+    private fun openFuture(name: ByteArray): CompletableFuture<SecureStorage> {
+        val completableFuture = CompletableFuture<SecureStorage>()
+        Executors.newCachedThreadPool().submit<Any?> {
+            Thread.sleep(100)
+            tables[ByteArrayWrapper(name)] = FakeStorage()
+            completableFuture.complete(tables[ByteArrayWrapper(name)]!!)
+            null
+        }
+        return completableFuture
+    }
+
+    override fun open(name: ByteArray): CompletableFuture<SecureStorage> {
+        return openFuture(name)
     }
 }
 
